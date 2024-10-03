cb324a4ca1e12223393e59d1fd44ee59d9c21cbb
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-10-03 20:11:53 +0300

    refactor(front): update theme switch

--- front/src/shared/ui/theme-switch.tsx
+++ front/src/shared/ui/theme-switch.tsx
@@ -18,7 +18,7 @@ export default function ThemeSwitch() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="size-12">
+        <Button variant="outline" size="icon" className="size-12 rounded-full">
           <SunIcon className="size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute size-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
