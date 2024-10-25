65ab6e942bad5a95c421731e5d4b432eeb2eb301
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-10-25 12:06:52 +0300

    feat: update modal image

--- front/src/shared/ui/modal-image.tsx
+++ front/src/shared/ui/modal-image.tsx
@@ -1,38 +1,38 @@
-'use client';
 import { ImageProps } from 'next/image';
-import { useState } from 'react';
 import { cn } from '../lib/utils';
 import CustomImage from './custom-image';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './dialog';
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
 
 export default function ModalImage({ src, alt, ...props }: ImageProps) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOpen = () => setIsOpen(true);
-  const handleClose = () => setIsOpen(false);
-
   return (
-    <>
-      <CustomImage
-        src={src}
-        alt={alt}
-        onClick={handleOpen}
-        {...props}
-        className={cn('cursor-pointer', props.className)}
-      />
-
-      {isOpen && (
-        <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75 cursor-pointer"
-          onClick={handleClose}
-        >
+    <Dialog>
+      <DialogTrigger>
+        <CustomImage
+          src={src}
+          alt={alt}
+          {...props}
+          className={cn('cursor-pointer', props.className)}
+        />
+      </DialogTrigger>
+      <DialogContent className="p-0 m-0 border-none bg-transparent flex items-center justify-center shadow-none">
+        <VisuallyHidden>
+          <DialogTitle>{alt}</DialogTitle>
+        </VisuallyHidden>
+        <DialogHeader className="max-h-[90vh] overflow-auto flex justify-center">
           <CustomImage
             src={src}
             alt={alt}
-            onClick={(e) => e.stopPropagation()}
-            className="object-contain max-w-[90%] max-h-[90%] relative rounded-lg cursor-default"
+            className="object-cover relative rounded-lg max-h-[90vh]"
           />
-        </div>
-      )}
-    </>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
