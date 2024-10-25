d0d33e28352068b0f947ea69f67d52dfa165ce10
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-10-25 08:50:52 +0300

    feat: add new email

--- api/src/mail/mail.service.ts
+++ api/src/mail/mail.service.ts
@@ -31,10 +31,28 @@ export class MailService {
   async sendVerifyEmailEvent({ to, token }: VerifyEmailPayload) {
     const mailOptions = {
       to,
-      subject: 'Verify your email',
-      html: `<a href="${this.config.get('CONFIRM_EMAIL_URL')}?token=${token}">Подтвердите ваш email</a>`,
+      subject: 'Подтвердите регистрацию в Grekasgram',
+      text: 'Подробности в письме',
+      html: `<!DOCTYPE html>
+              <html lang="ru">
+              <head>
+                  <meta charset="UTF-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                  <title>Подтверждение почты</title>
+              </head>
+              <body style="font-family: Arial, sans-serif; background-color: #f4f4f7; color: #333333; padding: 20px; text-align: center;">
+                  <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 30px; border-radius: 16px; box-shadow: 0 4px 8px rgba(0,0,0,0.1);">
+                      <h1 style="color: #7c3aed;">Подтвердите ваш адрес электронной почты</h1>
+                      <p>Спасибо за регистрацию в Grekasgram! Чтобы завершить процесс регистрации, пожалуйста, подтвердите свой адрес электронной почты, нажав на кнопку ниже.</p>
+                      <a href="${this.config.get('CONFIRM_EMAIL_URL')}?token=${token}"
+                        style="display: inline-block; margin: 20px auto; padding: 15px 30px; font-size: 18px; font-weight: bold; color: #ffffff; background-color: #7c3aed; text-decoration: none; border-radius: 12px;">
+                        Подтвердить почту
+                      </a>
+                      <p style="color: #555555;">Если вы не регистрировались в Grekasgram, просто проигнорируйте это письмо.</p>
+                  </div>
+              </body>
+              </html>`,
     };
-
     try {
       await this.transporter.sendMail(mailOptions);
     } catch (e) {
